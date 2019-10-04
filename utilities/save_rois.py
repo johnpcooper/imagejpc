0dd10548bc8791969af39e88cebe08fed481f6ce
johnpcooper <johnphilpcooper@gmail.com>
2019-10-04 11:49:28 -0500

    Added functionality to screen ROI set for positions with multiple ROIs and warn the user

--- utilities/save_rois.py
+++ utilities/save_rois.py
@@ -1,6 +1,9 @@
 from ij import IJ
 from ij.plugin.frame import RoiManager
 from ij.gui import Roi
+from ij.gui import GenericDialog
+
+gui = GenericDialog("ROI warning")
 
 # Get the active image, its title, and the directory where it lies
 imp = IJ.getImage()
@@ -20,3 +23,24 @@ if rois_count == N_imp_frames:
 
 else:
 	print("ROI array was {} frames long, active image was {} frames long".format(rois_count, N_imp_frames))
+	
+	# get the indices of the roi and let the user know which slices the double ROIs are at
+	rois_array = roim.getRoisAsArray()
+	double_roi_slices = []
+
+	for i in range(0, rois_count-1):
+	
+		slice_roi_n = rois_array[i].getZPosition()
+		slice_roi_n_plus_1 = rois_array[i+1].getZPosition()
+		slice_roi_n_min_1 = rois_array[i-1].getZPosition()
+	
+		if slice_roi_n == slice_roi_n_plus_1:
+			double_roi_slices.append(slice_roi_n)
+		elif slice_roi_n == slice_roi_n_min_1:
+			print("Multiple ROIs at position {}".format(slice_roi_n))
+			double_roi_slices.append(slice_roi_n)
+
+	for double_roi_slice in list(set(double_roi_slices)):
+		gui.addMessage("Multiple ROIs for slice {}".format(double_roi_slice))
+
+	gui.showDialog()
