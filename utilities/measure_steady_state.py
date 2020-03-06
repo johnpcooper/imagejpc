0fe19f362aa0936f5e324fe29555465e5cea4fa1
johnpcooper <johnphilipcooper@gmail.com>
2020-03-05 18:08:33 -0600

    Now automatically scans through multiple threshold percentiles and chooses most rois

--- utilities/measure_steady_state.py
+++ utilities/measure_steady_state.py
@@ -1,11 +1,14 @@
 import os
 from ij import IJ
 import glob
+from ij.plugin.frame import RoiManager
 
-_threshold_percent = IJ.getNumber("Enter proportion to cut off in brightfield edge segmentation", 0.85)
-_n_channels = IJ.getNumber("Enter proportion to cut off in brightfield edge segmentation", 3)
+_threshold_percent = 0.9
+_n_channels = IJ.getNumber("Enter the number of channels used in this experiment (including brightfield)", 3)
 
 def get_percentile(imp, percentile):
+
+	IJ.log("Segmenting brightfield with percentile threshold {}".format(percentile))
 
 	stats = imp.getRawStatistics()
 	hist = stats.histogram()
@@ -18,14 +21,14 @@ def get_percentile(imp, percentile):
 	
 	for bin in range(0, stats.nBins):
 	
-		print("Fraction pixels with intensity in bins 0 to {}".format(bin))
+		#print("Fraction pixels with intensity in bins 0 to {}".format(bin))
 		integral = sum(hist[0:bin])/sum(hist)
-		print(integral)
+		#print(integral)
 	
 		intensity = hist_x_range * bin/stats.nBins
 		hist_x_values.append(intensity)
 		
-		print(intensity)
+		#print(intensity)
 	
 		if integral > percentile:
 			threshold_intensity = intensity
@@ -50,9 +53,8 @@ def create_rois():
 	# Also scale the rois down using my plugin scale_rois.py
 	IJ.run("Set Measurements...", "area mean standard min centroid center perimeter bounding fit feret's integrated median stack display redirect=None decimal=3")
 	IJ.run("Analyze Particles...", "size=100-2000 circularity=0.60-1.00 exclude add in_situ")
-	IJ.run("scale rois", "choose=0.65 choose=0.65")
 
-def open_files(n_channels, threshold_percent):
+def analyze_files(n_channels, threshold_percent):
 	# Ask the user to choose a directory containing the images to be analyzed
 	# Open each one, find the brightfield image and process it to make ROIs
 	# then measure the ROIs, close that set and open the next
@@ -73,16 +75,46 @@ def open_files(n_channels, threshold_percent):
 
 			# find the brightfield image and process it
 			if 'bf' in imp.getTitle():
-				IJ.log("Found bf image")
-				threshold_brightfield(imp, threshold_percent)
+				# Closing the found bf image here cleans up the loop below
+				imp.close()
+				bf_path = path
+				IJ.log("Found bf image at {}".format(bf_path))				
+				# Try 14 different threshold values and use the one
+				# that yields the highest number of rois
+				roiCounts = []
+				threshold_values = []
+				for i in range(-15, 10):
+					IJ.open(bf_path)
+					imp = IJ.getImage()
+					threshold_value = threshold_percent + i*0.01
+					threshold_brightfield(imp, threshold_value)
+					create_rois()
+					
+					roim = RoiManager.getInstance()
+					roiCounts.append(roim.getCount())
+					threshold_values.append(threshold_value)
+
+					IJ.log("Found {} ROIs".format(roim.getCount()))
+					imp.changes = False
+					imp.close()
+					roim.reset()
+				# sorted() returns a sorted list of keys of
+				# the dictionary passed to the function
+				IJ.open(bf_path)
+				imp = IJ.getImage()
+				dic = dict(zip(roiCounts, threshold_values))
+				sorted_dic = sorted(dic)				
+				final_threshold_value = dic[sorted_dic[-1]]	
+				threshold_brightfield(imp, final_threshold_value)
 				create_rois()
+				IJ.run("scale rois", "choose=0.8 choose=0.8")
+				# Set the public variable changes to false so that 
+				# the save changes dialog won't pop up
+				imp.changes = False
+					
 			else:
 				pass
 		# Run my measure_rois.py plug in. Closes this set of images after running
-		IJ.run("measure rois")
+		IJ.run("measure and clear rois")
 		
-open_files(int(_n_channels), _threshold_percent)
-
-			
-			
-	
+analyze_files(int(_n_channels), _threshold_percent)
