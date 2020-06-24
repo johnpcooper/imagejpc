b101beb6802be17d39b4c6287409b158a055b140
johnpcooper <johnphilipcooper@gmail.com>
2020-06-24 15:15:30 -0500

    small updates, added save_rois.py to repo

--- utilities/save_rois.py
+++ utilities/save_rois.py
@@ -3,9 +3,6 @@ from ij.plugin.frame import RoiManager
 from ij.gui import Roi
 from ij.gui import GenericDialog
 
-# This is a script for saving roi sets define cell segmentation
-# in a cropped stack 
-
 gui = GenericDialog("ROI warning")
 
 # Get the active image, its title, and the directory where it lies
@@ -26,7 +23,7 @@ rois_count = roim.getCount()
 if rois_count == N_imp_frames:
     roi_save_path = str("{}{}_measurement_rois.zip".format(path, imp_title))
     roim.runCommand("Select All")
-    roim.runCommand("Save", roi_save_path)  
+    roim.runCommand("Save", roi_save_path)
     print("ROIs saved at {}".format(roi_save_path))
 
 else:
