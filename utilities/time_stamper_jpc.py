5bf5529225c6d9c98e9befd000e0e79c0b506208
johnpcooper <johnphilipcooper@gmail.com>
2020-10-25 19:31:08 -0500

    update time stamper

--- utilities/time_stamper_jpc.py
+++ utilities/time_stamper_jpc.py
@@ -5,6 +5,7 @@ imp = IJ.getImage()
 title = imp.getTitle()
 title = title[:-4]
 interval_units = IJ.getString("Collection interval units: ", 'hrs')
+start_time = IJ.getString("Start time: ", '0')
 interval_by_units = IJ.getNumber("Collection interval in {}:".format(interval_units), 0.17)
 fontsize = IJ.getNumber("Fontsize:", 14)
 #get the directory containing the active image
@@ -13,10 +14,10 @@ path = IJ.getDirectory("image")
 # will print as 'None'. If this is the case, have the user choose a directory to put the
 # timestamped tif
 if not path:
-	IJ.log("Image has no source directory")
-	path = IJ.getDirectory("Choose the directory to save the timestamped tif")
+    IJ.log("Image has no source directory")
+    path = IJ.getDirectory("Choose the directory to save the timestamped tif")
 else: 
-	pass
+    pass
 
 height = imp.getHeight()
 width = imp.getWidth()
@@ -28,10 +29,11 @@ IJ.log("Height: {}".format(height))
 IJ.log("Width: {}".format(width))
 IJ.log("Size: {}".format(size))
 
-IJ.run("Label...", "format=0 starting=0 interval={} x=0 y={} font={} text=[{}] range=1-{}".format(interval_by_units,
-																								  height,
-																								  fontsize,
-																								  interval_units,
-																								  size))
+IJ.run("Label...", "format=0 starting={} interval={} x=0 y={} font={} text=[{}] range=1-{}".format(start_time,
+                                                                                                   interval_by_units,
+                                                                                                   height,
+                                                                                                   fontsize,
+                                                                                                   interval_units,
+                                                                                                   size))
 
 IJ.saveAs("Tiff", "{}{}_time_stamped".format(path, title))
